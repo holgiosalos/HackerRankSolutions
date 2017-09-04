cabdf7033a04b0b0239af05a19929a828492a557
Holmes Salazar <hgsosorio@gmail.com>
2017-09-04 15:33:31 -0500

    Adding Time Conversion Challenge

---
+++ algorithms/warmup/time-conversion/src/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String timeConversion(String s) {
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String result = timeConversion(s);
+        System.out.println(result);
+    }
+}
