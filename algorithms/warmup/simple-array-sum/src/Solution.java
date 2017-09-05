8516dc7924ce801015701e4284e0b2565df6fc01
Holmes Salazar <hgsosorio@gmail.com>
2017-09-05 15:55:07 -0500

    Implementing solution for Array Sum problem

--- algorithms/warmup/simple-array-sum/src/Solution.java
+++ algorithms/warmup/simple-array-sum/src/Solution.java
@@ -1,23 +1,15 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
-    static int simpleArraySum(int n, int[] ar) {
-        // Complete this function
+  public static void main(String[] args) {
+    try (Scanner in = new Scanner(System.in)) {
+      int n = in.nextInt();
+      int result = 0;
+      for(int ar_i = 0; ar_i < n; ar_i++){
+        result += in.nextInt();
+      }
+      System.out.println(result);
     }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] ar = new int[n];
-        for(int ar_i = 0; ar_i < n; ar_i++){
-            ar[ar_i] = in.nextInt();
-        }
-        int result = simpleArraySum(n, ar);
-        System.out.println(result);
-    }
+  }
 }
